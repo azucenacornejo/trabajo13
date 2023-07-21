892fe55ca1474796b4bc36caeff3073f07c7b31e
azucenacornejo <cornejoquispeazucena@gmail.com>
2023-07-20 21:41:24 -0500

    Version 1 de sitio react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import C01component from './component/C01component';
+import C02contador from './component/C02contador';
+import C03component from './component/C03component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Componente 1</h1>
+      <C01component  />
+      <h1>Componente 2</h1>
+      <C02contador />
+      <h1>Componente 3</h1>
+      <C03component/>
     </div>
   );
 }
---
+++ src/src/C01componet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C01component = () => {
+  return (
+    <div>
+      <h1>Hola Mundo desde React</h1>
+    </div>
+  )
+}
+
+export default C01component
